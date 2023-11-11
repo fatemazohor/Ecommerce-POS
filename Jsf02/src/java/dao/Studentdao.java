e12abc8cdd1a480becf23ce4713b48d6f2823e86
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-11-11 13:16:25 +0600

    add

---
+++ Jsf02/src/java/dao/Studentdao.java
@@ -0,0 +1,22 @@
+package dao;
+
+import model.Students;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+public class Studentdao {
+
+    public void addStudent(Students s) {
+        Transaction tr = null;
+        Session se = HibernateUtil.getSessionFactory().openSession();
+        try {
+            tr = se.beginTransaction();
+            se.save(s);
+            tr.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
