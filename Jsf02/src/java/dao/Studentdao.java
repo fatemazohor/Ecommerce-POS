115c3b348bf54aee7953de2693cc20535852f7f4
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-11-13 12:50:13 +0600

    add13-11-23

--- Jsf02/src/java/dao/Studentdao.java
+++ Jsf02/src/java/dao/Studentdao.java
@@ -15,7 +15,7 @@ public class Studentdao {
         Session se = HibernateUtil.getSessionFactory().openSession();
         try {
             tr = se.beginTransaction();
-            se.save(s);
+            se.saveOrUpdate(s);
             tr.commit();
 
             return true;
@@ -37,23 +37,42 @@ public class Studentdao {
         return stuList;
     }
 
-    public List<Students> getStudentById(int id) {
-        List<Students> idList = new ArrayList<>();
-        Students stu = new Students();
-        Transaction tr = null;
-        Session se = HibernateUtil.getSessionFactory().openSession();
-
+    public Students getStudentById(int id) {
+        Session se=null;
+        Students stud=null;
         try {
-            tr = se.beginTransaction();
-            Query qu = se.createQuery("from Student where id= :id");
-            stu = (Students) qu.uniqueResult();
-            tr.commit();
+           se=HibernateUtil.getSessionFactory().openSession();
+           stud=(Students) se.get(Students.class, id);
+           
         } catch (Exception e) {
             e.printStackTrace();
-            tr.rollback();
+            
         } finally {
-            se.close();
+            try {
+                if (se !=null) {
+                    se.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
         }
-        return idList;
+        return stud;
     }
+    public boolean deleteStudent(int id){
+    Transaction tr=null;
+    Session se=HibernateUtil.getSessionFactory().openSession();
+        try {
+            tr=se.beginTransaction();
+            Students stud=(Students) se.load(Students.class, new Integer(id));
+            se.delete(stud);
+            tr.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            
+    return false;
+    }
+    
 }
